6d139daa951436e54f4d86c74977615db6f03194
justinjordan <justinjordan@justins-air>
2018-04-19 15:59:58 -0400

    database update.

--- FinalProject/src/classes/Flights.java
+++ FinalProject/src/classes/Flights.java
@@ -2,10 +2,13 @@ package classes;
 
 public class Flights {
 	// attributes from class 
+	private String flightID;
 	private String startCity = "";
 	private String endCity = "";
-	private String date = "";
-	private String time = "";
+	private String departureDate = "";
+	private String returnDate = "";
+	private String departureTime = "";
+	private String returnTime = "";
 	
 
 }
--- FinalProject/src/database/Database.java
+++ FinalProject/src/database/Database.java
@@ -2,23 +2,78 @@ package database;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.Statement;
 
 public class Database {
+	public static Connection conn;
 
 	public static void main(String[] args) throws Exception {
 		getConnection();
 
+		// INSERT DATA
+		try {
+
+			String sql = "insert into customer (socialSecurity, address, email, firstName, lastName, userName, flightsBooked,"
+					+ "state, zipCode,securityQuestion, paswword) values ('111223334', '123 dawson lane', 'jdoe@gmail.com', 'John', 'Doe', "
+					+ "'jdoe123', '5', 'GA', '30303', 'First pet?', 'gsustudent')";
+			PreparedStatement myStmt = conn.prepareStatement(sql);
+			myStmt.executeUpdate(sql);
+
+			System.out.println("Inserted Successfully");
+		} catch (Exception ex) {
+			ex.printStackTrace();
+			System.out.println("There is an error");
+		}
+		
+		// UPDATE DATA
+		try {
+
+			String sql = "update customer set email='doejohn42@yahoo.com' where socialSecurity=111223333";
+			PreparedStatement myStmt = conn.prepareStatement(sql);
+			myStmt.executeUpdate(sql);
+
+			System.out.println("Updated Successfully");
+		} catch (Exception ex) {
+			ex.printStackTrace();
+			System.out.println("There is an error");
+		}
+		
+		// DELETE DATA
+		try {
+
+			String sql = "delete from customer where socialSecurity=123231234";
+			PreparedStatement myStmt = conn.prepareStatement(sql);
+			myStmt.executeUpdate(sql);
+
+			System.out.println("Deleted Successfully");
+		} catch (Exception ex) {
+			ex.printStackTrace();
+			System.out.println("There is an error");
+		}
+		
+		//SELECT DATA
+		try {
+
+			String sql = "SELECT * from customer";
+			PreparedStatement myStmt = conn.prepareStatement(sql);
+			myStmt.executeQuery(sql);
+
+			System.out.println("DATA SELECTED:");
+		} catch (Exception ex) {
+			ex.printStackTrace();
+			System.out.println("There is an error");
+		}
 	}
 
 	public static Connection getConnection() throws Exception {
 		try {
 			String driver = "com.mysql.jdbc.Driver";
 			String url = "jdbc:mysql://localhost:3306/sys";
-			String username = "root";
-			String password = "Alexander1";
+
 			Class.forName(driver);
 
-			Connection conn = DriverManager.getConnection(url, username, password);
+			conn = DriverManager.getConnection(url);
 			System.out.println("Connected");
 			return conn;
 		} catch (Exception e) {
@@ -27,6 +82,10 @@ public class Database {
 
 		}
 		return null;
+	}
+
+	public static void InsertData(String firstName, String lastName) {
+
 	}
 
 }
