5f09bf5e58f89640a1c53554ebf8a891fec74748
justinjordan <justinjordan@justins-air>
2018-04-25 15:49:33 -0400

    Updates

--- FinalProject/src/application/Login.java
+++ FinalProject/src/application/Login.java
@@ -1,12 +1,15 @@
 package application;
 
 import database.Database;
+import classes.User;
 
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.util.ArrayList;
 
 import javax.swing.JOptionPane;
+import javax.swing.JScrollPane;
+import javax.swing.JTextArea;
 
 import javafx.application.Application;
 import javafx.geometry.Pos;
@@ -18,63 +21,34 @@ import javafx.scene.control.TextField;
 import javafx.scene.layout.GridPane;
 import javafx.stage.Stage;
 
+import application.Main;
 
-public class Login
-extends Application {
-  private TextField tfUsername= new TextField();
-  private TextField tfPassword = new TextField();
-  private Button btLogin = new Button("Login");
-  private Button btNewAccount = new Button("Create New Account");
+
+public class Login extends User {
+
+  public static Button btLogin = new Button("Login");
+  public static Button btNewAccount = new Button("Create New Account");
   
-  @Override // Override the start method in the Application class
-  public void start(Stage primaryStage) {
-    // Create UI
-    GridPane gridPane = new GridPane();
-    gridPane.setHgap(5);
-    gridPane.setVgap(5);
-    gridPane.add(new Label("Username:"), 0, 0);
-    gridPane.add(tfUsername, 1, 0);
-    gridPane.add(new Label("Password:"), 0, 1);
-    gridPane.add(tfPassword, 1, 1);
-    gridPane.add(btLogin, 1, 5);
-    gridPane.add(btNewAccount, 0, 10);
-
-    // Set properties for UI
-    gridPane.setAlignment(Pos.CENTER);
-    tfUsername.setAlignment(Pos.BOTTOM_RIGHT);
-    tfPassword.setAlignment(Pos.BOTTOM_RIGHT);
-    GridPane.setHalignment(btLogin, HPos.RIGHT);
-    GridPane.setHalignment(btNewAccount, HPos.RIGHT);
-
-    // Process events
-    btLogin.setOnAction(e -> loginVerification());
-    //btNewAccount.setOnAction(e -> calculateLoanPayment());
-
-    // Create a scene and place it in the stage
-    Scene scene = new Scene(gridPane, 400, 250);
-    primaryStage.setTitle("J&J Airlines Login"); // Set title
-    primaryStage.setScene(scene); // Place the scene in the stage
-    primaryStage.show(); // Display the stage
-  }
-  
-  private void loginVerification() {
+    
+  static void loginVerification() {
     // Get values from text fields
-	tfUsername.getText();
-    tfPassword.getText();
+	User.getTfUsername().getText();
+    User.getTfPassword().getText();
     
     try {
 		Database.getConnection();
 		
-		String sql = "select * from Customer where userName='" + tfUsername.getText() + 
-				"' and paswword='" + tfPassword.getText() + "'";
+		String sql = "select * from Customer where userName='" + User.getTfUsername().getText() + 
+				"' and password='" + User.getTfPassword().getText() + "'";
 		PreparedStatement myStmt = Database.conn.prepareStatement(sql);
 		
 		ResultSet rs = myStmt.executeQuery(sql);
-		if(rs.next())
-			JOptionPane.showMessageDialog(null, "Login Successful");
+		if (rs.next()) 
+			System.out.println("Login Successful");	
+
 		else
-			JOptionPane.showMessageDialog(null, "Incorrect username or password");
-		Database.conn.close();
+			System.out.println("Incorrect Username or Password");
+		Database.conn.close(); 
 	} catch (Exception e) {
 		// TODO Auto-generated catch block
 		e.printStackTrace();
@@ -86,9 +60,7 @@ extends Application {
    * The main method is only needed for the IDE with limited
    * JavaFX support. Not needed for running from the command line.
    */
-  public static void main(String[] args) {
-    launch(args);
-    
-  }
+
+
 }
 
